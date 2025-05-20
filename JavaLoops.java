46fc96f2ad0baf61cf805e991996336aea7d13b0
santhoshproprog <94008762+santhosh912003@users.noreply.github.com>
2025-05-20 12:51:07 +0530

    JavaLoops

--- JavaLoops.java
+++ JavaLoops.java
@@ -1,1 +1,10 @@
-
+import java.util.*;
+public class JavaLoops {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int N=scan.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(N+" x "+i+" = "+(N*i));
+        }
+    }
+}
