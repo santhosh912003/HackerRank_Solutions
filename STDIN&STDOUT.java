e2af18445fe2cab7de72f44b2eea195fa22661d6
santhoshproprog <94008762+santhosh912003@users.noreply.github.com>
2025-05-20 12:01:43 +0530

    Q2.STDIN&STDOUT

---
+++ STDIN&STDOUT.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int i=scan.nextInt();
+        int j=scan.nextInt();
+        int k=scan.nextInt();
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+    }
+}
